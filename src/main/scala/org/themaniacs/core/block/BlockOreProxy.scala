eb55f5edc39d6e733d11d8ad0e188a074898e912
Kilobyte22 <stiepen22@gmx.de>
2016-10-16 17:04:38 +0200

    Performed some code compacting

--- src/main/scala/org/themaniacs/core/block/BlockOreProxy.scala
+++ src/main/scala/org/themaniacs/core/block/BlockOreProxy.scala
@@ -4,6 +4,4 @@ import net.minecraft.block.BlockOre
 import org.themaniacs.core.block.extensions.Ore
 
 
-class BlockOreProxy(implementation: BlockBase with Ore) extends BlockOre(implementation.mapColor) with BlockProxy {
-  override val impl = implementation
-}
+class BlockOreProxy(override val impl: BlockBase with Ore) extends BlockOre(impl.mapColor) with BlockProxy
